7cb967a2db45d223a8d278899663c219d5fbf2e4
Foka tatsinkou Robert bright <fokrober@e1r8p4.1337.ma>
2019-06-18 23:17:03 +0100

    add valid_terimino.c

--- srcs/valid_tetrimino.c
+++ srcs/valid_tetrimino.c
@@ -6,7 +6,7 @@
 /*   By: fokrober <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/09 15:45:02 by fokrober          #+#    #+#             */
-/*   Updated: 2019/06/13 17:48:02 by fokrober         ###   ########.fr       */
+/*   Updated: 2019/06/18 22:56:57 by fokrober         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,29 +22,12 @@ int		is_inbounds(int *tab, int len, int ubound)
 	return (i == len);
 }
 
-int		chk(int *rel_pos, int *tab)
-{
-	int	i;
-	int	ret;
-
-	ret = 0;
-	i = 0;
-	while (!ret && i < 3)
-	{
-		if (rel_pos[i] == 1)
-			ret = (!(tab[0] % 4) || !(tab[1] % 4) || !(tab[2] % 4));
-		i++;
-	}
-	return (!ret);
-}
-
 int		istetris(int *rel_pos)
 {
 	static int	test[5][5][5];
 
 	if (!test[1][4][3])
 	{
-		ft_putstr("Initialisation du test de tetris...\n");
 		test[1][4][3] = 1;
 		test[1][4][4] = 1;
 		test[1][4][1] = 1;
@@ -70,6 +53,12 @@ int		valid_tetrimino(int *tab)
 	rel_pos[0] = tab[1] - tab[0];
 	rel_pos[1] = tab[3] - tab[2];
 	rel_pos[2] = tab[2] - tab[1];
+	if (!(tab[0] % 4) && rel_pos[0] == 1)
+		return (0);
+	if (!(tab[2] % 4) && rel_pos[1] == 1)
+		return (0);
+	if (!(tab[1] % 4) && rel_pos[2] == 1)
+		return (0);
 	sort(rel_pos, 2);
-	return (chk(rel_pos, tab) && is_inbounds(rel_pos, 3, 4) && istetris(rel_pos));
+	return (is_inbounds(rel_pos, 3, 4) && istetris(rel_pos));
 }
