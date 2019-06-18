a2191a837edd83cc8a1e3198388e36013a352854
Foka tatsinkou Robert bright <fokrober@e1r8p4.1337.ma>
2019-06-18 23:00:38 +0100

    norminette fixed

--- srcs/valid_file.c
+++ srcs/valid_file.c
@@ -6,11 +6,32 @@
 /*   By: fokrober <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/07 15:41:36 by fokrober          #+#    #+#             */
-/*   Updated: 2019/06/18 18:06:04 by fokrober         ###   ########.fr       */
+/*   Updated: 2019/06/18 22:59:36 by fokrober         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "valid_file.h"
+
+int		chk_line(char **line, int nblines, int *tab, size_t len)
+{
+	int		ret;
+
+	if (nblines % 5)
+	{
+		if ((ret = valid_line(*line, tab)) < 0)
+			return (ret_linedel(line, ret));
+	}
+	else if (!(**line))
+	{
+		if (isfull(tab, 4) != TAB_FULL || !valid_tetrimino(tab))
+			return (ret_linedel(line, NOT_TETRIS));
+		ft_bzero(tab, len);
+	}
+	else
+		return (ret_linedel(line, INV_LINE));
+	ft_strdel(line);
+	return (1);
+}
 
 int		valid_file(int fd)
 {
@@ -24,23 +45,12 @@ int		valid_file(int fd)
 	while ((ret = get_next_line(fd, &line)) > 0 && nblines != 129)
 	{
 		nblines++;
-		if (nblines % 5)
-		{
-			if ((ret = valid_line(line, tab)) < 0)
-				return (ret);
-		}
-		else if (!(*line))
-		{
-			ret = isfull(tab, 4);
-			if (ret != TAB_FULL || !valid_tetrimino(tab))
-				return (NOT_TETRIS);
-			ft_bzero(tab, sizeof(tab));
-		}
-		else
-			return (INV_LINE);
-		ft_strdel(&line);
+		ret = chk_line(&line, nblines, tab, sizeof(tab));
+		if (ret != 1)
+			return (ret);
 	}
-	if (!ret && !line)
+	if (!ret && !(nblines % 5))
 		return (valid_tetrimino(tab));
+	ft_strdel(&line);
 	return (ret);
 }
