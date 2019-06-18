218ce21f6d7affee60742b6d5ab45830995537ad
Foka tatsinkou Robert bright <fokrober@e1r8p4.1337.ma>
2019-06-18 18:06:47 +0100

    new valid_file with norme to fix

--- srcs/valid_file.c
+++ srcs/valid_file.c
@@ -6,7 +6,7 @@
 /*   By: fokrober <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/07 15:41:36 by fokrober          #+#    #+#             */
-/*   Updated: 2019/06/15 10:20:37 by fokrober         ###   ########.fr       */
+/*   Updated: 2019/06/18 18:06:04 by fokrober         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,24 +21,26 @@ int		valid_file(int fd)
 
 	ft_bzero(tab, sizeof(tab));
 	nblines = 0;
-	while ((ret = get_next_line(fd, &line)) > 0)
+	while ((ret = get_next_line(fd, &line)) > 0 && nblines != 129)
 	{
-		printf("ici");
 		nblines++;
-		if (nblines % 5 && (ret = valid_line(line, tab)) < 0)
-			return (ret);
-		else if (!(*line) && !(nblines % 5))
+		if (nblines % 5)
 		{
-			if (((ret = isfull(tab, 4)) != TAB_FULL) ||
-					(ret == TAB_FULL && !valid_tetrimino(tab)))
+			if ((ret = valid_line(line, tab)) < 0)
+				return (ret);
+		}
+		else if (!(*line))
+		{
+			ret = isfull(tab, 4);
+			if (ret != TAB_FULL || !valid_tetrimino(tab))
 				return (NOT_TETRIS);
 			ft_bzero(tab, sizeof(tab));
 		}
-		else if (*line && !(nblines % 5))
+		else
 			return (INV_LINE);
 		ft_strdel(&line);
 	}
-	if (!ret)
+	if (!ret && !line)
 		return (valid_tetrimino(tab));
 	return (ret);
 }
